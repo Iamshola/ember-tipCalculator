d5a2ba82e41a4669dcf3a38f2cc7b800e0347c04
Adesola Oni-Shogbonyo <adesola.oni-shogbony@MLH-0379.smart.lan>
2020-03-04 17:09:12 +0000

    updated the calculation logic

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,21 +5,21 @@ import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
   @tracked total = 0;
-  @tracked tipAmount = 0;
+  @tracked individualTip;
+
 
   constructor() {
     super(...arguments);
     this.options = ['0', '10', '20', '30', '40'];
     this.billAmount = 0;
     this.guestAmount = 0; 
-    this.tipAmount = 0;
+    this.individualTip = 0;
   }
   
-
   @action
   calculateAmount() {
-    this.tipAmount = Math.round(this.billAmount * this.tipAmount / 100) / 2
-    this.total = Number(this.tipAmount) + Number(this.billAmount) / Number(this.guestAmount);
+    this.individualTip = Math.round(this.billAmount * this.tipAmount / 100) / +(this.guestAmount)
+    this.total = Number(this.individualTip) + Number(this.billAmount);
   }
 
 }
